800a75f34e2e1a2bbc0f5803553caa4be4c9d215
Manny Navarro <manny.navarry@gmail.com>
2014-11-24 21:46:40 -0700

    added basic test to item_test file; populated sales_engine file with default data

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,2 +1,15 @@
 require_relative '../lib/item'
 require_relative 'test_helper'
+
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+
+class ItemTest < Minitest::Test
+
+  def test_item_exists
+    item = Item.new
+    assert item, item
+  end
+end
